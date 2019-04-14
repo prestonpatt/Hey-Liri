4ae69c4db05647ad9eb61ea66350e74305837a71
Antifreeze <preston.patterson1@gmail.com>
2019-04-14 14:41:14 -0500

    Minor updates

--- liri.js
+++ liri.js
@@ -25,11 +25,11 @@ function CommandInput(command, input) {
         case 'movie-this':
             movieFunction(input);
             break;
-        case 'do-what-it-says':
+        case 'does-what-it-says':
             doWhatFunction(input);
             break;
         default:
-            console.log("That's not valid. Enter one of the following: \n\nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says\n")
+            console.log("That's not valid. Enter one of the following: \n\nconcert-this \nspotify-this-song \nmovie-this\n")
     }
 }
 
@@ -45,17 +45,19 @@ function concertFunction(input) {
                         console.log("Couldn't find this artist. Try another artist")
                     } else {
                         for (var i = 0; i < response.data.length; i++) {
-                            var concertInfo = `\n
-                            Venue name: ${response.data[i].venue.name}\n
-                            Venue location: ${response.data[i].venue.city + ", " + response.data[0].venue.region}\n
-                            Event date: ${moment(response.data[i].datetime).format('L')}`
+                            var concertInfo = `${divider}
+Band name: ${input}\n
+Venue name: ${response.data[i].venue.name}\n
+Venue location: ${response.data[i].venue.city + ", " + response.data[0].venue.region}\n
+Event date: ${moment(response.data[i].datetime).format('L')}
+                            ${divider}`
                             console.log(concertInfo)
                             fs.appendFile('log.txt', concertInfo + divider, 'utf8', function (error) {
                                 if (error) {
                                     console.log("Couldn't append Data")
                                 }
-                                console.log("Data has been added to log.txt file.")
                             })
+                            console.log("Data has been added to log.txt file.")
                         }
 
                     }
@@ -76,11 +78,12 @@ function spotifyFunction() {
             console.log("Couldn't find your song.")
         }
         var results = data.tracks.items[0]
-        var spotifyInfo = (`\n
+        var spotifyInfo = (`${divider}
             Artist: ${results.artists[0].name}\n
             Song Title: ${results.name}\n
             Preview: ${results.preview_url}\n
-            Album: ${results.album.name}`);
+            Album: ${results.album.name}
+            ${divider} `)
         console.log(spotifyInfo)
         fs.appendFile('log.txt', spotifyInfo + divider, 'utf8', function (error) {
             if (error) {
@@ -101,7 +104,7 @@ function movieFunction() {
     axios
         .get(queryUrl).then(
             function (response) {
-                var movieInfo = (`\n
+                var movieInfo = (`${divider}
                 Title: ${response.data.Title}\n
                 Released: ${response.data.Year}\n
                 IMDB Rating: ${response.data.imdbRating}\n
@@ -109,7 +112,8 @@ function movieFunction() {
                 Country: ${response.data.Country}\n
                 Language: ${response.data.Language}\n
                 Plot: ${response.data.Plot}\n
-                Actors: ${response.data.Actors}`)
+                Actors: ${response.data.Actors}
+                ${divider}`)
                 console.log(movieInfo)
                 fs.appendFile('log.txt', movieInfo + divider, 'utf8', function (error) {
                     if (error) {
@@ -121,14 +125,14 @@ function movieFunction() {
 }
 
 function doWhatFunction() {
-    if (command === "do-what-it-says") {
+    if (command === "does-what-it-says") {
         fs.readFile('random.txt', 'UTF8', function (error, data) {
             if (error) {
                 console.log("I'm not sure what your saying.")
             }
             var dataArray = data.split(',');
             command = dataArray[0],
-            input = dataArray[1];
+                input = dataArray[1];
             CommandInput();
         });
     }
